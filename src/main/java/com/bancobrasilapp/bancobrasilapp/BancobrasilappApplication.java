ecdf9eca5aa65644992007c6e8b24190704e66aa
codedevjunior <code.devjunior@outlook.com.br>
2022-02-13 23:33:32 -0300

    Projeto Inicial

---
+++ src/main/java/com/bancobrasilapp/bancobrasilapp/BancobrasilappApplication.java
@@ -0,0 +1,13 @@
+package com.bancobrasilapp.bancobrasilapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancobrasilappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancobrasilappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bancobrasilapp/bancobrasilapp/BancobrasilappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancobrasilapp.bancobrasilapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancobrasilappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
